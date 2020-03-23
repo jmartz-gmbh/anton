e244bfb828acb0a5a44a5cdb071e65df4f1a2205
Jonathan Martz <git@jmartz.de>
2020-03-23 23:55:13 +0100

    update readme, optimize trigger

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 #### Anton Build Server
 
 ##### Description
-This is a simple Build Server which is my replacement for Jenkins with Robo
+This is a simple Build Server which is my replacement for Jenkins with Robo.
+For me Jenkins with Robo works well, but i want a light weight solution for it.
+So i will try to create a cli trigger build.
+Additional i will add a pwa to handle this and a lumen webhook to trigger builds via github push.
 
 ##### Security
 For Security reasons i would highly recommend you to run this server not public.
@@ -9,11 +12,19 @@ Only people in your network should have access to this server.
 If you need to work from outside of your company buiding, use a vpn to access the network.
 
 ##### Todo
-* initial setup with cli trigger
+* validate config in Collector
+* log errors and result in a build history
+* create logic to trigger builds by queue.json
+
+##### Later
 * add frontend with boilerplate
 * lumen api to trigger build via webhook
 * notify email
 * notify slack
 * notify nova ?
-* build queue
 * lumen only read access
+
+##### Classes
+Trigger - Class to trigger a build with project name and pipeline name
+Collector - Class to collect all projects and prepare workspace
+Queue - Class to trigger builds by queue.json
--- src/Trigger.php
+++ src/Trigger.php
@@ -3,86 +3,105 @@
 namespace Anton;
 
 class Trigger {
-
     public $project = '';
 
     public $branch = '';
 
+    public $build = 'failed';
+
     public function run(string $project, string $pipeline){
         $filename = 'anton.json';
 
-        if(file_exists($filename)){
-            $file = file_get_contents($filename);
-
-            $config = json_decode($file, true);
-            if(!empty($config[$project])){
-                $projectConfig = $config[$project];
-                $workdir = 'workspace/projects/'.$project;
-
-                if(file_exists($workdir) && is_dir($workdir)){
-                    if(!empty($projectConfig['pipelines'][$pipeline])){
-                        $branch = $projectConfig['pipelines'][$pipeline];   
-                        exec('cd '.$workdir.' && git checkout '.$branch);
+        try {
+            if(file_exists($filename)){
+                $file = file_get_contents($filename);
     
-                        $commits = exec('cd '.$workdir.' && git rev-list --count '.$branch);
-                        exec('cd '.$workdir.' && git pull');
+                $config = json_decode($file, true);
+                if(!empty($config[$project])){
+                    $projectConfig = $config[$project];
+                    $workdir = 'workspace/projects/'.$project;
     
-                        $steps = [];
-    
-                        try {
-                            if(count($projectConfig['steps']) > 0){
-                                exec('cd '.$workdir.' && mkdir -p .anton/log');
-                                foreach($projectConfig['steps'] as $key => $value){
-                                    $steps[$key] = [];
-                                    $steps[$key]['log'] = [];
-                                    $logfile = '.anton/log/'.$key.'.log';                    
-                                    exec('cd '.$workdir.' && robo '.$value . ' 2>&1 | tee '.$logfile);
-                                    
-                                    $steps[$key]['log']['exists'] = file_exists($workdir.'/'.$logfile);
-                    
-                                    if($steps[$key]['log']['exists']){
-                                        $steps[$key]['log']['text'] = trim(trim(file_get_contents($workdir.'/'.$logfile), PHP_EOL));
-                    
-                                        $steps[$key]['log']['exit'] = strpos($steps[$key]['log']['text'], 'Exit code ') !== false;
-                                        $steps[$key]['log']['exception'] = strpos($steps[$key]['log']['text'], '[error]') !== false;
-                                        // @todo catch ssh errors ?
-    
-                                        if($steps[$key]['log']['exception']){
-                                            throw new \Exception('Exception thrown while deployment.');
+                    if(file_exists($workdir) && is_dir($workdir)){
+                        if(!empty($projectConfig['pipelines'][$pipeline])){
+                            $branch = $projectConfig['pipelines'][$pipeline];   
+                            exec('cd '.$workdir.' && git checkout '.$branch);
+        
+                            $commits = exec('cd '.$workdir.' && git rev-list --count '.$branch);
+                            exec('cd '.$workdir.' && git pull');
+        
+                            $steps = [];
+        
+                            try {
+                                if(count($projectConfig['steps']) > 0){
+                                    exec('cd '.$workdir.' && mkdir -p .anton/log');
+                                    foreach($projectConfig['steps'] as $key => $value){
+                                        $steps[$key] = [];
+                                        $steps[$key]['log'] = [];
+                                        $logfile = '.anton/log/'.$key.'.log';                    
+                                        exec('cd '.$workdir.' && robo '.$value . ' 2>&1 | tee '.$logfile);
+                                        
+                                        $steps[$key]['log']['exists'] = file_exists($workdir.'/'.$logfile);
+                        
+                                        if($steps[$key]['log']['exists']){
+                                            $steps[$key]['log']['text'] = trim(trim(file_get_contents($workdir.'/'.$logfile), PHP_EOL));
+                        
+                                            $steps[$key]['log']['exit'] = strpos($steps[$key]['log']['text'], 'Exit code ') !== false;
+                                            $steps[$key]['log']['exception'] = strpos($steps[$key]['log']['text'], '[error]') !== false;
+                                            // @todo catch ssh errors ?
+        
+                                            if($steps[$key]['log']['exception']){
+                                                throw new \Exception('Exception thrown while deployment.');
+                                            }
+        
+                                            if($steps[$key]['log']['exit']){
+                                                throw new \Exception('Exit while deployment.');
+                                            }
                                         }
-    
-                                        if($steps[$key]['log']['exit']){
-                                            throw new \Exception('Exit while deployment.');
+                                        else{
+                                            throw new \Exception('Log not created. ('.$workdir.'/'.$logfile.')');
                                         }
-                                    }
-                                    else{
-                                        throw new \Exception('Log not created. ('.$workdir.'/'.$logfile.')');
                                     }
                                 }
                             }
-                        }
-                        catch (\Exception $e){
-                            die($e->getMessage());
-                        }
-                        
-                        // check if success
-                        exec('cd '.$workdir.' && robo check:build 2>&1 | tee .anton/log/check.log');
-    
-                        $check = file_get_contents($workdir. '/.anton/log/check.log');
-                        
-                        if($check === 'success'){
-                            exec('cd '.$workdir.' && rm -rf .anton/log');
-                            exec('rm -rf '.$workdir);
+                            catch (\Exception $e){
+                                die($e->getMessage());
+                            }
+                            
+                            // check if success
+                            exec('cd '.$workdir.' && robo check:build 2>&1 | tee .anton/log/check.log');
+        
+                            $check = file_get_contents($workdir. '/.anton/log/check.log');
+                            
+                            if($check === 'success'){
+                                exec('cd '.$workdir.' && rm -rf .anton/log');
+                                exec('rm -rf '.$workdir);
+                            }
+                            else{
+                                throw new \Exception('Step failed. ('.$key.')');
+                            }
                         }
                         else{
-                            echo 'Failed.'.PHP_EOL;
+                            throw new \Exception('Pipeline unknown. ('.$pipeline.')');
                         }
                     }
+                    else{
+                        throw new \Exception('Workdir doesnt exits.');
+                    }
+                }
+                else{
+                    throw new \Exception('Project unknown. ('.$project.')');
                 }
             }
+            else{
+                throw new \Exception('Anton.json missing.');
+            }
 
-            // set build as success
+            $this->build = 'success';
+            echo 'Build successfull';
+            
+        } catch(\Exception $e){
+            echo $e->getMessage().PHP_EOL;
+            exit(0);
         }
-
     }
 }
