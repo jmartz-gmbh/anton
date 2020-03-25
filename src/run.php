e69c5bdebd7fa5dab558e49d11822f18a3a14394
Jonathan Martz <git@jmartz.de>
2020-03-25 22:10:27 +0100

    replace classes with modules

--- src/run.php
+++ src/run.php
@@ -1,17 +1,14 @@
 <?php
 
-include('Build.php');
-include('Helper.php');
-include('Collector.php');
-include('Jobber.php');
+require_once('vendor/autoload.php');
 
 if(count($argv) == 2 && $argv[1] == 'collect'){
     $collector = new \Anton\Collector();
     $collector->run();
 }
 elseif(count($argv) == 2 && $argv[1] == 'jobber'){
-    $collector = new \Anton\Jobber();
-    $collector->run();
+    $jobber = new \Anton\Jobber();
+    $jobber->run();
 }
 elseif(count($argv) == 4 && $argv[1] == 'trigger'){
     $trigger = new \Anton\Build($argv[2],$argv[3]);
@@ -21,6 +18,8 @@ else{
     echo 'Anton Help. usage:'.PHP_EOL;
     echo 'to collect Anton config do -> ';
     echo './anton.sh collect'.PHP_EOL;
+    echo 'to execute Jobs in queue do -> ';
+    echo './anton.sh jobber'.PHP_EOL;
     echo 'to trigger Anton build do -> ';
     echo './anton.sh trigger [project] [branch]'.PHP_EOL;
 }
